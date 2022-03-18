61278988165196b36894899d2354c11de328f848
Nigel Martis <nigelmartis0@gmail.com>
2022-03-18 07:47:05 -0400

    Update

--- UrbanAQ.py
+++ UrbanAQ.py
@@ -3,8 +3,7 @@ import numpy as np
 import PIL as pil
 import streamlit as st
 import plotly.express as px
-import plotly.graph_objects as go
-
+import hydralit as hy
 
 st.set_page_config(page_title="Urban AQ", layout= "wide")
 
